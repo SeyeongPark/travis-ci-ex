7ec41dbed573e4e11b5aa06a415a257521398131
SeyeongPark <lynn.yeong.102@gmail.com>
2021-04-09 11:22:28 -0400

    travis ci support

---
+++ src/test/java/com/techprimers/ci/travisciex/TravisCiExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.ci.travisciex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
